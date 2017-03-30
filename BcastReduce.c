959c328c9feaa9e354d7cbba846e4f3e22979077
pascaltll <jctuesta@uni.pe>
2017-03-30 13:41:07 -0500

    Update BcastReduce.c

--- BcastReduce.c
+++ BcastReduce.c
@@ -5,9 +5,11 @@ MPI_Comm_size(MPI_COMM_WORLD,&numprocs);//el programa solo encia un valor a cada
 .....
 MPI_Bcast(&data,1,MPI_INT,0,MPI_COMM_WORLD);//separa , 0 es desde donde en vias la informacion 
 ....
+ //MPI_barrier -> hacxe que el proceso anterior termine, es un abarrera, garantiza un sincronismo
 MPI_reduce(&data,&res,DATA_SIZE,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);//une los procesos res es donde se guada el resultado
 .....//DATA_SIZE-> int -> es el numero de procesos, tamaño de datos
   //MPI_SUM ->tipo de operacion
   //0-> desde el proceso que esta
+  //MPI_COMM_WORLD -> para paritcionar los procesos
 MPI_Finalize();
 }
