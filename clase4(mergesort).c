c050c95094d056c0577dc65cce7a98160bf5d0a0
Juan Carlos Tuesta <jctuesta@uni.pe>
2017-04-06 20:29:13 +0200

    Create clase4(mergesort).c
    secuencia

---
+++ clase4(mergesort).c
@@ -0,0 +1,18 @@
+///secuencial
+void merge_sort(int iArray[], int startIndex, int endIndex){
+  int midIndex;
+ //check for base case
+ if(startIndex >= endIndex) / 2{
+  return;
+  }
+//first, divide is half
+midIndex = (startIndex + endIndex);
+
+//first recursive call
+merge_sort(iArray, startIndex, midIndex);
+
+//second recursive call
+merge_sort(iArray, midIndex, endIndex);
+
+//the merge funcion
+merge(iArray,startIndex, endIndex);
